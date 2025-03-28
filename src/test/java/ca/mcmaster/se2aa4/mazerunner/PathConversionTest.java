f8a59d8a4fecce5bfb9b1f5507e4cba0b0ea74b1
Andy Pak <im.andy.pak@gmail.com>
2025-03-27 22:54:40 -0400

    Write tests for conversion between canonical and factorized paths, closing #5

---
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/PathConversionTest.java
@@ -0,0 +1,37 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import ca.mcmaster.se2aa4.mazerunner.Path.CanonicalPath;
+import ca.mcmaster.se2aa4.mazerunner.Path.FactorizedPath;
+import ca.mcmaster.se2aa4.mazerunner.Path.Path;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class PathConversionTest {
+    private static final Logger logger = LogManager.getLogger();
+
+    @Test
+    public void testFromCanonicalToFactorized() {
+        Path p = new CanonicalPath();
+        for (int i = 0; i < 4; i++) {
+            p.add('F');
+        }
+        assertEquals("FFFF", p.toString());
+        String factorizedP = FactorizedPath.convertToFactorized(p.toString());
+        assertEquals("4F", factorizedP.toString());
+    }
+
+    @Test
+    public void testFromFactorizedToCanonical() {
+        Path p = new FactorizedPath();
+        for (int i = 0; i < 4; i++) {
+            p.add('F');
+        }
+        assertEquals("4F", p.toString());
+        String canonicalP = CanonicalPath.convertToCanonical(p.toString());
+        assertEquals("FFFF", canonicalP.toString());
+    }
+}
