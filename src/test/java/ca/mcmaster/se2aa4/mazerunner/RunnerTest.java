3e45f91443062ed107b66495532fcb662f5d04fb
Andy Pak <im.andy.pak@gmail.com>
2025-03-30 20:39:54 -0400

    Update Runner unit tests to account for new changes

--- src/test/java/ca/mcmaster/se2aa4/mazerunner/RunnerTest.java
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/RunnerTest.java
@@ -3,34 +3,45 @@ package ca.mcmaster.se2aa4.mazerunner;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import ca.mcmaster.se2aa4.mazerunner.Navigation.Direction;
 import ca.mcmaster.se2aa4.mazerunner.Navigation.Location;
 import ca.mcmaster.se2aa4.mazerunner.Runner.Runner;
+import ca.mcmaster.se2aa4.mazerunner.Solver.RightHandSolver;
+import ca.mcmaster.se2aa4.mazerunner.Solver.Subject;
 
 public class RunnerTest {
+    private Subject subject;
+
+    @BeforeEach
+    public void setUp() {
+        this.subject = new RightHandSolver();
+    }
+
     @Test
     public void testIsNotNull() {
-        Runner r = Runner.getInstance();
+        Runner r = Runner.getInstance(subject);
         assertNotNull(r);
     }
 
     @Test
     public void testIsSingleton() {
-        Runner r1 = Runner.getInstance();
-        Runner r2 = Runner.getInstance();
+        Runner r1 = Runner.getInstance(subject);
+        Runner r2 = Runner.getInstance(subject);
         assertSame(r1, r2);
     }
 
     @Test
     public void testLocation() {
-        Runner r = Runner.getInstance();
+        Runner r = Runner.getInstance(subject);
         r.setLocation(new Location(0, 0));
         assertEquals(new Location(0, 0), r.getLocation());
     }
 
     @Test
     public void testDirection() {
-        Runner r = Runner.getInstance();
+        Runner r = Runner.getInstance(subject);
         r.setDirection(Direction.RIGHT);
         assertEquals(Direction.RIGHT, r.getDirection());   
     }
